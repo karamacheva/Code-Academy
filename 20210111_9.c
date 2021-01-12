25aefc9ae7480e52f20e5f7f353dab0b59273315
Ekaterina Karamacheva <76935364+karamacheva@users.noreply.github.com>
2021-01-12 13:32:14 +0200

    Update 20210111_9.c

--- 20210111_9.c
+++ 20210111_9.c
@@ -4,7 +4,7 @@ int main(void){
     int number;
     printf("Enter a number: ");
     scanf("%d",&number);
-    int mask = 0b10001111;;
+    int mask = 0b10001111;
     int result = number & mask;
     printf("The new number is: %d",result);
 
