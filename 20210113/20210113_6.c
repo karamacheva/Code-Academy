aedf9fa5a81d1901398f39bc40533d5c6cb99a75
Ekaterina Karamacheva <76935364+karamacheva@users.noreply.github.com>
2021-01-14 02:18:53 +0200

    Update 20210113_6.c

--- 20210113/20210113_6.c
+++ 20210113/20210113_6.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
 int main(void){
-    short int a=-127;
+    char a=-127;
     printf("%d",a);
-    unsigned short int b=255;
+    unsigned char b=255;
     printf("\n%d",b);
     unsigned int c=6237498;
     printf("\n%d",c);
     unsigned int d=4294967292;
     printf("\n%u",d);
     long long int e=-9000000000000775845;
-     printf("\n%lld",e); 
+    printf("\n%lld",e); 
     return 0;
 }
