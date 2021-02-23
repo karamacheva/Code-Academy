1787c0c5aec2e3fa3478c106def2f6864435013b
Ekaterina Karamacheva <76935364+karamacheva@users.noreply.github.com>
2021-02-23 14:52:53 +0200

    Delete 20210222_15.c

--- 20210222/20210222_15.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-/*Дефинирайте структура с 4 полета (int. char[10], double, enum),
-направете масив от 20 структури и ги попълнете. Изведете масива
-от структури на стандартния изход в CSV формат (
-https://en.wikipedia.org/wiki/Comma-separated_values ). Пример:
-prog1 > structs20.cvs*/
