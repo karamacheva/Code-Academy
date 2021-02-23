c6757a4f2ca2eea8f60e8eaba49e8b5778fd5fa1
Ekaterina Karamacheva <76935364+karamacheva@users.noreply.github.com>
2021-02-23 11:32:47 +0200

    Add files via upload

---
+++ 20210222/20210222_15.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+/*Дефинирайте структура с 4 полета (int. char[10], double, enum),
+направете масив от 20 структури и ги попълнете. Изведете масива
+от структури на стандартния изход в CSV формат (
+https://en.wikipedia.org/wiki/Comma-separated_values ). Пример:
+prog1 > structs20.cvs*/
