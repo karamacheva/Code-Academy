9cae7ae31ac837e35c4c5104a3251bc103bcf3f7
Ekaterina Karamacheva <76935364+karamacheva@users.noreply.github.com>
2021-05-13 08:41:56 +0300

    Add files via upload

---
+++ StarcraftError/include/ProtossShip.h
@@ -0,0 +1,26 @@
+#ifndef ProtossShip_H_
+#define ProtossShip_H_
+
+#include "Defines.h"
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include "TerranShip.h"
+
+typedef struct terranShip terranShip;
+
+typedef struct protossShip{
+    int protossHP;
+    int protossShield;
+    int protossDMG;
+    enum AirShipType shipType;
+} protossShip;
+
+protossShip *initPhoenix(protossShip *airShip);
+protossShip *initCarrier(protossShip *airShip);
+int carrierInteceptors(protossShip *airShip);
+protossShip *damageTaken(protossShip *pShip,terranShip *tShip);
+protossShip *regenerateShield(protossShip *pShip);
+
+#endif /* ProtossShip_H_ */
